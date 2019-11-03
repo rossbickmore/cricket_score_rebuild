64e69d625f91109146b03fa61242ee40a442529c
ROSS BICKMORE <rossbickmore@gmail.com>
2019-11-03 15:30:05 +0000

    added toast notifications

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { updateScore, updateTeam } from '../reducers/scoreReducer';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Panel({ store }) {
   const state = store.getState();
   const team = state.teamBatting;
+  const notify = () => toast("Wow so easy !")
 
   const onTeamChange = (action) => {
     alert('You have changed the team to ' + action)
@@ -11,11 +14,13 @@ function Panel({ store }) {
   }
 
   const onScoreChange = (action, team) => {
+    toast(action + ' runs! good going ' + team + ' team')
     store.dispatch(updateScore(action, team))
   }
 
   return (
     <div>
+      <ToastContainer autoClose={2000}/>
       <button onClick={() => onTeamChange("home")}>Home</button>
       <button onClick={() => onTeamChange("away")}>Away</button>
       <button onClick={() => onScoreChange("one", team)}>1</button>
